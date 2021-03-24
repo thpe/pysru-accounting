8a60ad313671b7299ff0403e4337da7137b47c1f
Thomas Petig <thomas@petig.eu>
2021-03-24 13:26:34 +0100

    made code more pep8 complient

--- pysru_accounting/blankettsru.py
+++ pysru_accounting/blankettsru.py
@@ -1,30 +1,39 @@
 import re
 import numpy as np
 import pandas as pd
+from datetime import datetime
+
 
 class BlankettSRU:
-    def __init__ (self):
-        print ('init')
+    def __init__(self):
+        print('init')
         self.lines = []
+        self.upp   = []
+        self.blankett = ''
+        self.postcode = ''
 
-    def info (self, info):
+    def info(self, info):
         self.info = info
 
-    def id (self, nr):
+    def orgnr (self, org):
+        self.org = org
+        self.id = self.org + datetime.today().strftime(' %Y%m%d %H%M%S')
+
+    def id(self, nr):
         self.id = nr
 
-    def name (self, name):
+    def name(self, name):
         self.name = name
 
-    def uppgift (self, u):
+    def uppgift(self, u):
         self.upp.append(u)
 
-    def print (self):
-        print ('#BLANKETT '+self.blankett)
-        print ('#IDENTITET '+self.id)
-        print ('#SYSTEMINFO '+self.info)
-        for upp in self.uppgift:
-            print ('#UPPGIFT '+upp)
-        print ('#POSTNR '+self.postcode)
-        print ('#BLANKETTSLUT')
-        print ('#FIL_SLUT')
+    def print(self):
+        print('#BLANKETT '   + self.blankett)
+        print('#IDENTITET '  + self.id)
+        print('#SYSTEMINFO ' + self.info)
+        for u in self.upp:
+            print('#UPPGIFT ' + u)
+        print('#POSTNR ' + self.postcode)
+        print('#BLANKETTSLUT')
+        print('#FIL_SLUT')
