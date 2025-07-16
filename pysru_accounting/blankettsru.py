3b1adbb829e3d50e0df01a630750b32c0c64ab6a
Thomas Petig <thomas@petig.eu>
2025-07-17 01:54:42 +0200

    make pylint happy

--- pysru_accounting/blankettsru.py
+++ pysru_accounting/blankettsru.py
@@ -1,60 +1,69 @@
-import re
-import numpy as np
-import pandas as pd
+""" The PySRU blankett """
 from datetime import datetime
 
 
 class BlankettSRU:
+    """ a SRU file with one, or more blanketts """
     def __init__(self):
-        self.lines = []
         self.upp   = []
         self.blktt = ''
-        self.prog = 'PySRU'
+        self.prog  = 'PySRU'
         self.content = ''
 
         self.aname = ''
-        self.org  = ''
-        self.id   = ''
+        self.org   = ''
+        self._identity   = ''
 
-    def blankett(self, v):
-        self.blktt = v
+    def blankett(self, blktt):
+        """ set blankett field """
+        self.blktt = blktt
 
     def orgnr (self, org):
+        """ set organisation number """
         self.org = org
-        self.id = self.org + datetime.today().strftime(' %Y%m%d %H%M%S')
+        self._identity = self.org + datetime.today().strftime(' %Y%m%d %H%M%S')
 
-    def id(self, nr):
-        self.id = nr
+    def identity(self, number):
+        """ set id """
+        self._identity = number
 
     def name(self, aname):
+        """ set author name """
         self.aname = aname
 
     def clear_uppgift(self):
+        """ clear the data elements under uppgift """
         self.upp = []
 
-    def uppgift(self, u):
-        self.upp.append(u)
+    def uppgift(self, uppgift):
+        """ add an uppgift """
+        self.upp.append(uppgift)
 
     def uppgift_num(self, sru, val):
+        """ add an uppgift """
         self.upp.append(str(sru) + ' ' + str(int(val)))
 
     def print(self):
+        """ print the generated content """
         print(self.content)
 
     def generate(self):
+        """ generate for the current blankett """
         self.content += f'#BLANKETT {self.blktt}\n'
-        self.content += f'#IDENTITET {self.id}\n'
+        self.content += f'#IDENTITET {self._identity}\n'
         self.content += f'#SYSTEMINFO {self.prog}\n'
-        for u in self.upp:
-            self.content = self.content + '#UPPGIFT ' + u + '\n'
+        for uppgift in self.upp:
+            self.content = self.content + '#UPPGIFT ' + uppgift + '\n'
         self.content = self.content + '#BLANKETTSLUT' + '\n'
         return self.content
 
     def finish_file(self):
+        """ finish the file for writing """
         self.content = self.content + '#FIL_SLUT'
         return self.content
 
     def write(self, filename):
+        """ write the content to the file """
         self.content = self.finish_file().encode('iso-8859-1')
-        with open(filename, 'wb') as f:
-            f.write (self.content)
+        with open(filename, 'wb') as file:
+            file.write (self.content)
--- pysru_accounting/infosru.py
+++ pysru_accounting/infosru.py
@@ -1,47 +1,60 @@
-import re
-import numpy as np
-import pandas as pd
+""" writer for the info.sru file """
 
 
 class InfoSRU:
+    """ class for writing the file """
     def __init__(self):
+        """ init """
         print('init')
         self.lines = []
+        self._filename = ''
+        self._town = ''
+        self._postcode = ''
+        self._name = ''
+        self._orgnr = ''
 
     def filename(self, name):
-        self.filename = name
+        """ set the filename """
+        self._filename = name
 
-    def organisation_nr(self, nr):
-        self.orgnr = nr
+    def organisation_nr(self, orgnr):
+        """ set organisation number """
+        self._orgnr = orgnr
 
     def name(self, name):
-        self.name = name
+        """ set author name """
+        self._name = name
 
     def postcode(self, postcode):
-        self.postcode = postcode
+        """ set postcode """
+        self._postcode = postcode
 
     def town(self, town):
-        self.town = town
+        """ set town """
+        self._town = town
 
     def print(self):
+        """ print the output """
         print (self.generate())
 
     def generate(self):
+        """ generate the file content """
         string = ''.join([
                  '#DATABESKRIVNING_START'    , '\n',
                  '#PRODUKT SRU'              , '\n',
                  '#PROGRAM pysru-accounting' , '\n',
-                 '#FILNAMN ' , self.filename , '\n',
+                 '#FILNAMN ' , self._filename , '\n',
                  '#DATABESKRIVNING_SLUT'     , '\n',
                  '#MEDIELEV_START'           , '\n',
-                 '#ORGNR '   , self.orgnr    , '\n',
-                 '#NAMN '    , self.name     , '\n',
-                 '#POSTNR '  , self.postcode , '\n',
-                 '#POSTORT ' , self.town     , '\n',
+                 '#ORGNR '   , self._orgnr   , '\n',
+                 '#NAMN '    , self._name    , '\n',
+                 '#POSTNR '  , self._postcode, '\n',
+                 '#POSTORT ' , self._town    , '\n',
                  '#MEDIELEV_SLUT'            , '\n'])
         return string
 
     def write(self, filename):
+        """ write the file """
         content = self.generate().encode('iso-8859-1')
-        with open(filename, 'wb') as f:
-            f.write (content)
+        with open(filename, 'wb') as file:
+            file.write (content)
